a37ae3316570867afd8885dc2b11b1bebc6fc2aa
Steven Sim <stevensim226@gmail.com>
2022-07-28 15:36:56 +0700

    Add required_providers

--- vpc/data.tf
+++ vpc/data.tf
@@ -1,3 +1,13 @@
+terraform {
+  required_version = ">= 1.2.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.70"
+    }
+  }
+}
+
 provider "aws" {
     region = "ap-southeast-3"
     alias = "aws_jkt"
