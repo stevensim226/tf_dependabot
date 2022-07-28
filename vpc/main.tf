bc3115f0e1c33819ee98b860a84da8c8de503c37
Steven Sim <stevensim226@gmail.com>
2022-07-28 16:06:00 +0700

    Try using new module

--- vpc/main.tf
+++ vpc/main.tf
@@ -1,4 +1,58 @@
-resource "aws_vpc" "main" {
-  provider   = aws.aws_jkt
-  cidr_block = "10.0.0.0/16"
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "3.11.0"
+
+  name = "hello"
+  cidr = "20.10.0.0/16" # 10.0.0.0/8 is reserved for EC2-Classic
+
+  azs                 = ["ap-southeast-3a", "ap-southeast-3b", "ap-southeast-3c"]
+  private_subnets     = ["20.10.1.0/24", "20.10.2.0/24", "20.10.3.0/24"]
+  public_subnets      = ["20.10.11.0/24", "20.10.12.0/24", "20.10.13.0/24"]
+  database_subnets    = ["20.10.21.0/24", "20.10.22.0/24", "20.10.23.0/24"]
+  elasticache_subnets = ["20.10.31.0/24", "20.10.32.0/24", "20.10.33.0/24"]
+  redshift_subnets    = ["20.10.41.0/24", "20.10.42.0/24", "20.10.43.0/24"]
+  intra_subnets       = ["20.10.51.0/24", "20.10.52.0/24", "20.10.53.0/24"]
+
+  create_database_subnet_group = false
+
+  manage_default_route_table = true
+  default_route_table_tags   = { DefaultRouteTable = true }
+
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  enable_classiclink             = true
+  enable_classiclink_dns_support = true
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+
+  customer_gateways = {
+    IP1 = {
+      bgp_asn     = 65112
+      ip_address  = "1.2.3.4"
+      device_name = "some_name"
+    },
+    IP2 = {
+      bgp_asn    = 65112
+      ip_address = "5.6.7.8"
+    }
+  }
+
+  enable_vpn_gateway = true
+
+  enable_dhcp_options              = true
+  dhcp_options_domain_name         = "service.consul"
+  dhcp_options_domain_name_servers = ["127.0.0.1", "10.10.0.2"]
+
+  # Default security group - ingress/egress rules cleared to deny all
+  manage_default_security_group  = true
+  default_security_group_ingress = []
+  default_security_group_egress  = []
+
+  # VPC Flow Logs (Cloudwatch log group and IAM role will be created)
+  enable_flow_log                      = true
+  create_flow_log_cloudwatch_log_group = true
+  create_flow_log_cloudwatch_iam_role  = true
+  flow_log_max_aggregation_interval    = 60
 }
