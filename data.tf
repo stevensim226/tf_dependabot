f2eee5eef290bdc16e6ecf9334ac8ffab13a9409
Steven Sim <stevensim226@gmail.com>
2022-07-28 15:49:49 +0700

    Remove provider specific version

--- data.tf
+++ data.tf
@@ -11,11 +11,9 @@ terraform {
 provider "aws" {
     region = "ap-southeast-3"
     alias = "aws_jkt"
-    version = "~> 3.70"
 }
 
 provider "aws" {
     region = "ap-southeast-1"
     alias = "aws_sg"
-    version = "~> 3.70"
 }
--- vpc/data.tf
+++ vpc/data.tf
@@ -11,5 +11,4 @@ terraform {
 provider "aws" {
     region = "ap-southeast-3"
     alias = "aws_jkt"
-    version = "~> 3.70"
 }
